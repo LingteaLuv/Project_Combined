de162afa9be6f0bbe3d0d0113217cb012a403dbb
01stone01 <hagwhr2@gmail.com>
2025-06-20 15:45:01 +0900

    [Feat] Add CameraController script
    #11 백뷰 시점 카메라 구현

---
+++ Assets/LSW/CameraController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Drag&Drop")] [SerializeField] private GameObject _player;
+    [SerializeField] private float _rotateSpeed;
+
+    private Vector3 _offset;
+    private float _curRotationX;
+    private float _curRotationY;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void LateUpdate()
+    {
+        SetCameraPosition();
+        SetCameraRotation();
+    }
+
+    private void SetCameraPosition()
+    {
+        Vector3 backOffset = _player.transform.TransformDirection(_offset);
+        Vector3 desiredPos = _player.transform.position + backOffset;
+        transform.position = desiredPos;
+    }
+
+    private void SetCameraRotation()
+    {
+        transform.LookAt(_player.transform.position);
+    }
+    
+    private void Init()
+    {
+        _offset = new Vector3(0, 3, -6);
+    }
+}
