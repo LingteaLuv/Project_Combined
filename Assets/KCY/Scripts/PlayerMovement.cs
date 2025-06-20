555c4938dc751e6a04e78545c4cc540a67974c90
ch-yun38 <kscy0328@gmail.com>
2025-06-20 17:42:10 +0900

    [Feat] Add climbing stairs(1/2)
    - 플레이어 캐릭터의 계단을 오르는 로직을 구현

--- Assets/KCY/Scripts/PlayerMovement.cs
+++ Assets/KCY/Scripts/PlayerMovement.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Reference")]
     public Rigidbody Rb;
+    public Transform PlayerPos;
     //public Animation Avatar;
 
 
@@ -20,8 +22,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]private Transform _jumpCheck; // 배열로 전환하고, 물체 확인해서 여러 오브젝트에 대응하도록
     [SerializeField]private LayerMask _groundMask;
     private float _jumpDis = 0.3f;
-    
 
+    // 계단
+    [SerializeField] private Transform _bottmRayOrigin;
+    [SerializeField] private Transform _upperRayOrigin;
 
     // 현재 가속 저장
     private float _curAccel;
@@ -29,14 +33,12 @@ public class PlayerMovement : MonoBehaviour
     private bool _isJumped = false;
     
 
-
-
     public void SetMove(Vector3 dir)
     {
         // 현 속도 확인
         Vector3 move = Rb.velocity;
         // 설정 속도
-        Vector3 vec = dir * _moveSpeed;
+        Vector3 vec = dir.normalized * _moveSpeed;
 
         // 점프 시 점프 가속도로 전환하고 점프 중 이동하는 속도를 조절한다.
         _curAccel = _isJumped ? _jumpAccel : _moveAccel;
@@ -62,9 +64,28 @@ public class PlayerMovement : MonoBehaviour
         Rb.velocity = jumpVel;
     }
 
+    public void ClimbStairs()
+    {
+        // 벽에다 플레이어의 전뱡향에 대해 빛을 쏘고 아래의 레이는 막히고, 위의 레이에서는 막히지 않는 경우 계단으로 인식
+        Vector3 stairsRay = Vector3.forward;
+        bool bottomRay = Physics.Raycast(_bottmRayOrigin.position, stairsRay, 0.4f, _groundMask);
+        bool upperRay = Physics.Raycast(_bottmRayOrigin.position, stairsRay, 0.4f, _groundMask);
+
+        // 계단으로 인식하는 것에 성공했을 경우 upper레이가 있는 쪽 만큼 올려줌
+        if (bottomRay && !upperRay)
+        {
+            //Rb.position += 
+        }
+
+    }
+
+
+
+
     private void Awake()
     {
         Rb = GetComponent<Rigidbody>();
+        PlayerPos = GetComponent<Transform>();
         //Avatar = GetComponent<Animation>();
     }
 
