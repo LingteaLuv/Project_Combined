0f47cc5a8e1a757b3ec4c1584e56afdb3b55c0ce
munsung143 <akunta3475@gmail.com>
2025-06-20 13:49:35 +0900

    [Feat] 인벤토리 UI 외형 구성

---
+++ Assets/KMS/ItemSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "ScriptableObjects/Item", order = 1)]
+public class ItemSO : ScriptableObject
+{
+    [SerializeField] private Sprite _sprite;
+    public Sprite Sprite { get { return _sprite; } }
+
+    [SerializeField] [TextArea] private string _description;
+    public string Description { get { return _description; } }
+
+    [SerializeField] private int _maxInventoryAmount;
+    public int MaxInventoryAmount { get { return _maxInventoryAmount; } }
+}
