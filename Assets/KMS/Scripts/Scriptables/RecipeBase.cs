fa8d849ed69abccb1342921386bafd4088f72425
munsung143 <akunta3475@gmail.com>
2025-06-24 21:26:10 +0900

    [Feat] 크래프팅 UI구성

---
+++ Assets/KMS/Scripts/Scriptables/RecipeBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "new recipe", menuName = "ScriptableObjects/recipe", order = 1)]
+public class MyData : ScriptableObject
+{
+}
