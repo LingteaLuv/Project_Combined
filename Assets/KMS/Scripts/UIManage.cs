cc1218213bc15faa230e23af6f47c88419352a86
munsung143 <akunta3475@gmail.com>
2025-06-24 10:37:14 +0900

    [Feat] 루팅 UI 구성 수정

--- Assets/KMS/Scripts/UIManage.cs
+++ Assets/KMS/Scripts/UIManage.cs
@@ -2,55 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum ModalUI
+{
+    nothing,
+    inventory,
+    lootTable
+}
 public class UIManage : SingletonT<UIManage>
 {
     [SerializeField] public GameObject ModalBase;
     [SerializeField] public GameObject LootUI;
     [SerializeField] public GameObject InvUI;
 
-    public enum ModalUI
-    {
-        inventory,
-        lootTable
-    }
+    private RectTransform _lootRect;
+    private RectTransform _invRect;
     private void Awake()
     {
+        _lootRect = LootUI.GetComponent<RectTransform>();
+        _invRect = InvUI.GetComponent<RectTransform>();
         SetInstance();
         IsModalUIOpened = false;
+        Current = ModalUI.nothing;
     }
 
     public bool IsModalUIOpened { get; set; }
     public ModalUI Current { get; set; }
-    public void OpenUI()
+    public void OpenUI(ModalUI cur)
     {
         if (IsModalUIOpened) return;
         IsModalUIOpened = true;
         ModalBase.SetActive(true);
+        Current = cur;
         if (Current == ModalUI.inventory)
         {
-            LootUI.SetActive(true);
-            SetUIPos(LootUI.transform, 1000, 600);
+            InvUI.SetActive(true);
+            SetUIPos(_invRect, 750, 450);
         }
         else if (Current == ModalUI.lootTable)
         {
             LootUI.SetActive(true);
-            SetUIPos(LootUI.transform, 500, 600);
+            SetUIPos(_lootRect, 500, 600);
             InvUI.SetActive(true);
-            SetUIPos(InvUI.transform, 1300, 600);
+            SetUIPos(_invRect, 1100, 450);
         }
 
     }
-    public void CloseModalUI()
+    public void CloseUI()
     {
         if (!IsModalUIOpened) return;
         IsModalUIOpened = false;
+        Current = ModalUI.nothing;
         LootUI.SetActive(false);
         InvUI.SetActive(false);
         ModalBase.SetActive(false);
     }
-
-    private void SetUIPos(Transform UITrs, int x, int y)
+    public void ToggleUI(ModalUI cur)
     {
-        UITrs.position.Set(x, y, 0);
+        if (Current == cur)
+        {
+            CloseUI();
+        }
+        else
+        {
+            OpenUI(cur);
+        }
+
+
+    }
+
+    private void SetUIPos(RectTransform UITrs, int x, int y)
+    {
+        UITrs.position = new Vector3(x, y, 0); 
     }
 }
