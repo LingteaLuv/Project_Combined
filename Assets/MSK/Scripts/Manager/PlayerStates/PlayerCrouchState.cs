6b3887c06540fd2c89345be79acd9aec49264411
KyuSSeo <kyuri2665@gmail.com>
2025-06-27 10:46:42 +0900

    [Fix] Crouch Colilder Fix
    Colilder를 두개를 분리하여 상단의 Colilder를 CrouchState진입 시 비활성화 하도록 하였습니다.
    사용하지 않는 테스트 맵을 지웠습니다

--- Assets/MSK/Scripts/Manager/PlayerStates/PlayerCrouchState.cs
+++ Assets/MSK/Scripts/Manager/PlayerStates/PlayerCrouchState.cs
@@ -15,6 +15,8 @@ public class PlayerCrouchState : PlayerState
         {
             _movement.Controller.IsCrouch = true;
         }
+        if (_movement.CrouchCollider != null)
+            _movement.CrouchCollider.enabled = false;
         _movement.Controller._animator.SetBool("IsCrouch", true);
     }
 
@@ -22,6 +24,8 @@ public class PlayerCrouchState : PlayerState
     {
         _movement.Controller._animator.SetBool("IsCrouch", false);
         _movement.SetCrouch(false);
+        if (_movement.CrouchCollider != null)
+            _movement.CrouchCollider.enabled = true;
     }
 
     public override void Tick()
