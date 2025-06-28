2920880095f73723722ca1c00f67ce19048b602b
01stone01 <hagwhr2@gmail.com>
2025-06-28 18:52:02 +0900

    [Feat] Fix camera rotation
    #125 카메라 y축 회전 시점 고정 적용, 방망이 위치 수정

--- Assets/MSK/Scripts/Manager/PlayerCameraController.cs
+++ Assets/MSK/Scripts/Manager/PlayerCameraController.cs
@@ -8,16 +8,24 @@ public class PlayerCameraController : MonoBehaviour
     //[SerializeField] private Transform _lookatTarget;
     //[SerializeField] private Transform _fallowTarget;
     [SerializeField] public CinemachineBrain _cinemachineBrain;
-
+    [SerializeField] private Transform _cameraRig;
+    [SerializeField] private float _minPitch;
+    [SerializeField] private float _maxPitch;
 
     [Header("Mouse Config")]
-    [SerializeField][Range(0, 5)] private float _mouseSensitivity = 1;
+    [SerializeField][Range(0, 1)] private float _mouseSensitivityX;
 
-    public float Offset { get; private set; }
+    [SerializeField] [Range(0, 1)] private float _mouseSensitivityY;
+
+    public float OffsetX { get; private set; }
+    public float OffsetY { get; private set; }
     
     private void Update()
     {
-        Offset += Input.GetAxis("Mouse X") * _mouseSensitivity;
+        OffsetX += Input.GetAxis("Mouse X") * _mouseSensitivityX;
+        OffsetY += Input.GetAxis("Mouse Y") * _mouseSensitivityY;
+        OffsetY = Mathf.Clamp(OffsetY, _minPitch, _maxPitch); 
+        _cameraRig.localEulerAngles = new Vector3(OffsetY, 0f, 0f);
     }
 
     public void PauseCamera()
