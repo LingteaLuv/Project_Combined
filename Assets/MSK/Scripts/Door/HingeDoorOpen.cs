0be3ad183ac0f458a03c11c931f393b98118d5dc
KyuSSeo <kyuri2665@gmail.com>
2025-06-30 16:10:42 +0900

    [Fix] Door Angle Fix
    문 열고 닫히는 회전 축 수정

--- Assets/MSK/Scripts/Door/HingeDoorOpen.cs
+++ Assets/MSK/Scripts/Door/HingeDoorOpen.cs
@@ -1,8 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class HingeDoorOpen : MonoBehaviour, IInteractable
 {
@@ -24,8 +21,32 @@ public class HingeDoorOpen : MonoBehaviour, IInteractable
     }
     private void Init()
     {
+        // DoorType에 따라 닫힌 상태의 기준 회전값 지정
+        float closedY = 0f;
+        switch (_doorType)
+        {
+            case DoorType.RotateRight:
+                closedY = 180f;
+                break;
+            case DoorType.RotateLeft:
+                closedY = 0f;
+                break;
+            case DoorType.Slide:
+                // 슬라이드 문은 각도 적용 없음
+                break;
+        }
+
+        // 문 오브젝트의 Y축 회전을 기준 각도로 맞춤
+        Vector3 euler = transform.eulerAngles;
+        euler.y = closedY;
+        transform.eulerAngles = euler;
+
+        // 기준(닫힌) 회전값 저장
         _closedRotation = transform.rotation;
-        _openedRotation = _closedRotation * Quaternion.Euler(0, _openAngle * (_doorType == DoorType.RotateRight ? 1 : -1), 0);
+
+        // 열린 상태의 회전값도 같은 방식으로 계산
+        float openAngle = _doorType == DoorType.RotateRight ? _openAngle : -_openAngle;
+        _openedRotation = _closedRotation * Quaternion.Euler(0, openAngle, 0);
     }
     public void Interact()
     {
