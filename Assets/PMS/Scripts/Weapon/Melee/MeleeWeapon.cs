369a9e571220e900b1672d47cea38c589b58d05a
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-07-01 00:05:14 +0900

    [Fix] 플레이어 참조
    플레이어 참조 변경

--- Assets/PMS/Scripts/Weapon/Melee/MeleeWeapon.cs
+++ Assets/PMS/Scripts/Weapon/Melee/MeleeWeapon.cs
@@ -46,7 +46,6 @@ public class MeleeWeapon : WeaponBase
     /// Physics.OverlapSphere + 범위 + 애니메이션 Event를 통한 특정 프레임 이벤트 호출, 각도 체크X - 무기기준 
     /// 추후 콜라이더 변경으로 각도가 해결되지 않을 경우에 플레이어 기준으로 각도체크 하는 부분 추가하면 될 것 같다.
     /// </summary>
-
     public override void Attack()
     {
         //무기에 달려있는 _attack를 중심으로 범위를 설정하고 타겟레이어와 충돌검사
