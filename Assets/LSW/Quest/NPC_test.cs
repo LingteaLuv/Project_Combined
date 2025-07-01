6f97a2a3748a79ea9eba9ee3da664557f472aa07
01stone01 <hagwhr2@gmail.com>
2025-07-01 21:57:17 +0900

    [Feat] Add quest scripts
    퀘스트 기능 관련 스크립트 작성

---
+++ Assets/LSW/Quest/NPC_test.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_test : MonoBehaviour
+{
+    [Header("Drag&Drop")] 
+    [SerializeField] private NPCSO _data;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void Init()
+    {
+        
+    }
+}
