6e52deefed7bd042d3dd616fa91d9de06998d49a
munsung143 <akunta3475@gmail.com>
2025-07-03 17:18:37 +0900

    [Fix] 사용아이템 관련 수정중

---
+++ Assets/KMS/Scripts/New Folder/UseETC.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IUseETC
+{
+    public void UseETCItem();
+}
+
+
+public class UseETC : MonoBehaviour, IUseETC
+{
+    public void UseETCItem()
+    {
+
+    }
+}
