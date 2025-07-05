5d59ebf44a6e45001df7e8a0bd834596eaa85134
munsung143 <akunta3475@gmail.com>
2025-07-05 12:33:36 +0900

    [Fix] 타임라인 수정

--- Assets/KMS/Scripts/New Folder/TomatoTimeline.cs
+++ Assets/KMS/Scripts/New Folder/TomatoTimeline.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class TomatoTimeline : TimelineControl
 {
+    [SerializeField] string _startNPCID;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.F5))
         {
             _pd.Play();
+        }
+    }
+    private void Start()
+    {
+        //_pni.OnInteract3 += StartTL;
+    }
+    private void StartTL(string id)
+    {
+        if (!_hasRun && id == _startNPCID)
+        {
+            _hasRun = true;
+            _pd.Play();
+            StartCoroutine(DisableControl());
         }
     }
 }
